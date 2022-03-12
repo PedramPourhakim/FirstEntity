5bd72713af4d76a49784b3c10204162db8728448
PedramPourhakim <shemuel1226@gmail.com>
2022-03-12 18:21:36 +0330

    Connection Strings & Database Launch Settings

--- FirstEntity/FirstEntityProject/Startup.cs
+++ FirstEntity/FirstEntityProject/Startup.cs
@@ -1,6 +1,8 @@
+using EfFirstLibrary.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,9 @@ namespace FirstEntityProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddRazorPages();
+            services.AddDbContext<EfContext>(x=>x.UseSqlServer(Configuration.GetConnectionString("Izak")));
             services.AddControllersWithViews();
         }
 
@@ -51,6 +56,7 @@ namespace FirstEntityProject
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
