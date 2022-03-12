82fcbf91c65d42fa4cb16fd5ff04256f40219290
PedramPourhakim <shemuel1226@gmail.com>
2022-03-12 18:09:26 +0330

    EfContext Class For DbSets Added

---
+++ FirstEntity/EfFirstLibrary/Context/EfContext.cs
@@ -0,0 +1,28 @@
+﻿using EfFirstLibrary.Mapping;
+using EfFirstLibrary.ProductCategory;
+using EfFirstLibrary.TheProduct;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfFirstLibrary.Context
+{
+    public class EfContext :DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public EfContext(DbContextOptions<EfContext> options):base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(ProductMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
