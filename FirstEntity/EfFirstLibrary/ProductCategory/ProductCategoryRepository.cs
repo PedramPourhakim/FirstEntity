6ca83fd506b08b3b60e1d09d9c2a14843c2161c7
PedramPourhakim <shemuel1226@gmail.com>
2022-03-12 18:40:56 +0330

    Edit Category Class & Implementation Of I ProductCategory Repository Added Named ProductCategory Repository

---
+++ FirstEntity/EfFirstLibrary/ProductCategory/ProductCategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using EfFirstLibrary.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfFirstLibrary.ProductCategory
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private readonly EfContext efContext;
+        public ProductCategoryRepository(EfContext efContext)
+        {
+            this.efContext = efContext;
+        }
+        public void Create(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CategoryViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditCategory GetDetails(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            efContext.SaveChanges();
+        }
+
+        public List<CategoryViewModel> Search(string Name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
