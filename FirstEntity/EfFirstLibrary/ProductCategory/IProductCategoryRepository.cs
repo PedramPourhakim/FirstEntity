aced54931485af2927aeed57dbe3529f09b1073e
PedramPourhakim <shemuel1226@gmail.com>
2022-03-12 18:33:21 +0330

    IproductCategory Repository & Category View Model Added

---
+++ FirstEntity/EfFirstLibrary/ProductCategory/IProductCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfFirstLibrary.ProductCategory
+{
+    public interface IProductCategoryRepository
+    {
+        public void Get(int Id);
+        public void Create(Category category);
+        public bool Exists(string name);
+        public void SaveChanges();
+        public EditCategory GetDetails(int Id);
+        public List<CategoryViewModel> Search(string Name);
+        public List<CategoryViewModel> GetAll();
+    }
+}
